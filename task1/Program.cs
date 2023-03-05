2abe339c49572d7a1cb0cba14f162ad7bcb3117c
Aleksandr <shbragnikov@inbox.ru>
2023-03-05 20:22:06 +0300

    1

---
+++ task1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Ввод с клавиатуры М чисел. Посчитать, сколько чисел больше 0 введено
+// Считать число с консоли
+int Prompt(string message)
+{
+    System.Console.Write(message); // вывести сообщение
+    string value = Console.ReadLine(); //считывает с консоли строку
+    int result = Convert.ToInt32(value); //преобразует строку в целое число
+    return result; //возвращает результат
+}
+// Ввести массив
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
